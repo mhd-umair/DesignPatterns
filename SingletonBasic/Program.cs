2e09d50687c51407adcb331f0ad4dba1cd0b2c97
mhd-umair <77413987+mhd-umair@users.noreply.github.com>
2024-09-06 16:43:35 -0500

    Add files via upload

---
+++ SingletonBasic/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SingletonBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance1 = Singleton.GetInstance();
+            Singleton instance2 = Singleton.GetInstance();
+
+            instance1.ShowMessage("Hello from Singleton!");
+
+            // Check if instance1 and instance2 are the same
+            if (instance1 == instance2)
+            {
+                Console.WriteLine("Both instances are the same.");
+            }
+        }
+    }
+}
---
+++ SingletonBasic/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonBasic
+{
+    class Singleton
+    {
+        // The single instance of the class is stored in this static field.
+        private static Singleton _instance = null;
+
+        // Private constructor prevents instantiation from other classes.
+        private Singleton()
+        {
+            Console.WriteLine("Singleton instance created");
+        }
+
+        // Public method to provide access to the instance.
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        public void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
